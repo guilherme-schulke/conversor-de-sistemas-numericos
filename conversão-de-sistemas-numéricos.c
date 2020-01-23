f89c5f83a3d6e7137d426bd4af31f4cff73ae0a1
guilherme-schulke <58240853+guilherme-schulke@users.noreply.github.com>
2020-01-23 12:02:58 -0300

    alteração de menu
    feito um menu para escolha de opções para a conversão

--- conversão-de-sistemas-numéricos.c
+++ conversão-de-sistemas-numéricos.c
@@ -3,9 +3,26 @@
 #include <stdlib.h>
 #include <tgmath.h>
 
+void menu(void)
+{
+
+
+puts("\t\tBem vindo ao conversor\t\t\n");
+puts("1-\tDe qualquer base para a decimal\n");
+puts("2-\tDa decimal para qualquer base\n");
+puts("3-\tBinario para Octal e Octal para Binario\n");
+puts("4-\tBinario para Hexadecimal e Hexadecimal para Binario\n");
+puts("5-\tOctal para Hexadecimal e Hexadecimal para Octal\n");
+puts("0-\tsair\n");
+
+}
+
+
+
+
 int main(void){
 
-
+menu();
 
 
 
